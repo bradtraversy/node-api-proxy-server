b1e799960fff394b93f091c0fa4f810356b86c26
Brad Traversy <traversymedia@gmail.com>
2021-10-20 08:05:35 -0400

    Fetch from public API

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/api', (req, res) => {
-  res.json({ success: true })
-})
-
 // Enable cors
 app.use(cors())
+
+// Routes
+app.use('/api', require('./routes'))
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const url = require('url')
+const express = require('express')
+const router = express.Router()
+const needle = require('needle')
+
+// Env vars
+const API_BASE_URL = process.env.API_BASE_URL
+const API_KEY_NAME = process.env.API_KEY_NAME
+const API_KEY_VALUE = process.env.API_KEY_VALUE
+
+router.get('/', async (req, res) => {
+  try {
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query,
+    })
+
+    const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+    const data = apiRes.body
+
+    // Log the request to the public API
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+    }
+
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+})
+
+module.exports = router
