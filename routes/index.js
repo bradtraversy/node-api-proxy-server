ea52e91af3bfbab5b2c1fa6a799be4646152dcb3
Brad Traversy <traversymedia@gmail.com>
2021-10-20 08:07:53 -0400

    Added caching

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@ const url = require('url')
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
+const apicache = require('apicache')
 
 // Env vars
 const API_BASE_URL = process.env.API_BASE_URL
 const API_KEY_NAME = process.env.API_KEY_NAME
 const API_KEY_VALUE = process.env.API_KEY_VALUE
 
-router.get('/', async (req, res) => {
+// Init cache
+let cache = apicache.middleware
+
+router.get('/', cache('2 minutes'), async (req, res) => {
   try {
     const params = new URLSearchParams({
       [API_KEY_NAME]: API_KEY_VALUE,
