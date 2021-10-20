31fd6768c3c50701d3b08edbe56712cb019d1ff2
Brad Traversy <traversymedia@gmail.com>
2021-10-20 08:06:22 -0400

    Added rate limiting

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 Mins
+  max: 100,
+})
+app.use(limiter)
+app.set('trust proxy', 1)
 
 // Enable cors
 app.use(cors())
