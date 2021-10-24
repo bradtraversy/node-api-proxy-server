ff8313596c6eb6602595a237a11275928767e7cb
xkiller0 <med.ennahas@gmail.com>
2021-10-24 14:13:11 +0000

    chnage limit to 50

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 Mins
-  max: 100,
+  max: 50,
 })
 app.use(limiter)
 app.set('trust proxy', 1)
